0fea3c3bf3ca91c58444eb2e936e71576fe6b11e
jhovana22 <portillarj.aprim18@gmail.com>
2020-10-11 16:06:52 -0500

    Sepalo Jhovana

---
+++ SEPALO.R
@@ -0,0 +1,18 @@
+
+#Tema: Tabla de frecuencias
+
+#1.-Leer la matriz de datos
+data(iris)
+
+#2.- Exploramos la matriz de datos
+names(iris)
+iris#3.- Explorar la dimension de la matriz 
+dim(iris)
+
+#4.- Identificar los tipos de variables
+str(iris)
+
+#5.- Detectar valores perdidos
+anyNA(iris)
+
+#6.- explorar una variable
