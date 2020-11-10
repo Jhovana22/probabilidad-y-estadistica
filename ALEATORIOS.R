d61a373e6b68974cdd4c606b3c65406737f8ea2e
Jhovana22 <72088716+Jhovana22@users.noreply.github.com>
2020-11-10 11:07:39 -0600

    Generador de números aleatorios (MAS)
    Muestreo aleatorio simple.

---
+++ ALEATORIOS.R
@@ -0,0 +1,14 @@
+
+#generar numero aleatorios para muestra de 35 personas en una
+#poblacion de 73 alumnas/os del 5to semestre de la LEP BENV
+
+sample(x, side, replace=True/False)
+
+#Ejemplo
+sample(1:73,35, replace = FALSE)
+
+#generar numero aleatorios para una muestra de 34 personas en una 
+#poblacion de 70 alumnas/os del 7mo semestre de la LEP BENV
+
+sample(1:70,34, replace = FALSE)
+sample(1:70,34, replace = TRUE)
