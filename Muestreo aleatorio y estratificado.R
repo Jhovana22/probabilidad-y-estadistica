c6ee8ed402373aa44ce4101f9e8525b4a66a2311
Jhovana22 <72088716+Jhovana22@users.noreply.github.com>
2020-11-24 19:27:04 -0600

    Muestreo aleatorio y estratificado

---
+++ Muestreo aleatorio y estratificado.R
@@ -0,0 +1,70 @@
+#Generar numero aleatorios para una muestra de 35 personas en una
+#población de 73 alumnas/os del 5to semestre de la LEP BENV
+
+sample(x, side, replace=True/FALSE)
+
+sample(1:73,35, replace = FALSE)
+sample(1:70,34, replace=FALSE)
+
+#Calcula el tamaño de la muestra para una poblacion de
+#83 alumnas y alumnos que se encuentran en primer grado
+#de la Licenciatura en Educacion Primaria
+sample(1:83,55, replace=FALSE)
+
+#1.2.- Una vez que obtengas el tamaño de la muestra,
+#selecciona el numero de lista de las personas que formaran
+#parte de de la muestra
+(14, 51, 76, 15, 40, 27, 21, 68, 11, 20,  1, 73, 80, 13, 57, 25, 72, 63, 75, 36, 29, 8, 66, 47, 17, 46, 60,
+49,  3, 52, 74, 77, 45, 71, 50, 65, 37, 58, 61, 62, 41, 59, 64, 10, 2, 83, 12, 54, 56, 55, 38, 19, 78, 34,
+43)
+#1.3.- Crea un vector llamado muestra con los números de 
+#lista de las personas que forman parte de la muestra.
+muestraJho<-c(14, 51, 76, 15, 40, 27, 21, 68, 11, 20,  1, 73, 80, 13, 57, 25, 72, 63, 75, 36, 29, 8, 66, 47, 17, 46, 60,
+              49,  3, 52, 74, 77, 45, 71, 50, 65, 37, 58, 61, 62, 41, 59, 64, 10, 2, 83, 12, 54, 56, 55, 38, 19, 78, 34,
+              43)
+#2.- Repite el ejercicio anterios ahora para una poblacion
+# de 90 alumnas y alumnos de integran el segundo año de la
+#licenciatura en Educación Primaria de la BENV.
+sample(1:90,59, replace= FALSE)
+#Numero de listas
+(47, 29, 17, 23, 35, 45, 20, 84, 70, 79, 53, 27, 63, 59, 39, 78, 55, 19, 49,  6, 58, 72, 56,  2, 77, 10, 11, 85, 74, 18, 21,
+81, 89, 46, 15, 36, 82,  8, 16, 26, 61,  5, 25, 13, 44, 64,  3, 24, 54, 9,  7, 31,  1, 42, 90, 14, 37, 48, 52)
+
+#Vector
+muestraJho<-c(47, 29, 17, 23, 35, 45, 20, 84, 70, 79, 53, 27, 63, 59, 39, 78, 55, 19, 49,  6, 58, 72, 56,  2, 77, 10, 11, 85, 74, 18, 21,
+              81, 89, 46, 15, 36, 82,  8, 16, 26, 61,  5, 25, 13, 44, 64,  3, 24, 54, 9,  7, 31,  1, 42, 90, 14, 37, 48, 52)
+#3.- Determina el número e identifíca a las personas que participarán
+#en un estudio de seguimiento de egresados de la generación
+#2015-2019 de las 5 licenciaturas de la BENV.
+
+#Primaria: 78 personas egresadas.
+#Preescolar: 84 personas egresadas.
+#Física: 35 personas egresadas.
+#Telesecundaria: 83 personas egresadas.
+#Especial: 25 personas.
+sample(1:305,94, replace=FALSE)
+
+(175, 138, 174, 106, 229, 218,  65,  26,
+239, 194,  95,  91, 122, 195,  20, 289,
+105,  58, 171, 179, 115, 114, 256, 113,
+52, 199, 149, 111,  43,  68, 266, 107,
+21, 255,  50,  72, 200, 109, 151,  84,
+53, 214, 209,  75,  39,  25, 296,  44,
+16,  78, 189, 277, 226, 147,  36,  66,
+176, 41,  96, 119,  38, 183, 250, 300,
+172, 298,  47, 148, 245, 143, 134, 241,
+146, 276,  71, 273,  79, 164,  81, 131,
+124, 272, 264, 156, 259, 244, 240,  34,
+184, 177,  22, 118, 242, 208)
+muestraJho<-c(175, 138, 174, 106, 229, 218,  65,  26,
+              239, 194,  95,  91, 122, 195,  20, 289,
+              105,  58, 171, 179, 115, 114, 256, 113,
+              52, 199, 149, 111,  43,  68, 266, 107,
+              21, 255,  50,  72, 200, 109, 151,  84,
+              53, 214, 209,  75,  39,  25, 296,  44,
+              16,  78, 189, 277, 226, 147,  36,  66,
+              176, 41,  96, 119,  38, 183, 250, 300,
+              172, 298,  47, 148, 245, 143, 134, 241,
+              146, 276,  71, 273,  79, 164,  81, 131,
+              124, 272, 264, 156, 259, 244, 240,  34,
+              184, 177,  22, 118, 242, 208)
